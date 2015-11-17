36295ff6566c988cb21744ffd3bc5fa7d633513e
KhalilC <klcrumpler@gmail.com>
2015-11-17 02:03:13 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smokey', 5, 'male', 'red', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smokey1', 5, 'male', 'red', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smokey2', 5, 'male', 'red', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smokey3', 5, 'male', 'red', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smokey4', 5, 'male', 'red', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smokey5', 5, 'male', 'red', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smokey6', 5, 'male', 'red', 'happy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, 'male', 'red', 'happy', 0);
