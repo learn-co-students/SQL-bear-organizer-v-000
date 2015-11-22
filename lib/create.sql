8ab3e06f0fd315b7d3096c739cf423da06e3bedf
onyoo <roberto9m9@gmail.com>
2015-11-22 13:06:11 +0100

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  color TEXT,
+  gender TEXT,
+  temperament TEXT,
+  alive TEXT
+  );
---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+
+
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES (
+    NULL, 5, "orange", "M", "fun-loving", "yes");
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES (
+    "Minsu", 3, "red", "M", "grumpy", "yes");
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES (
+    "Nabi", 8, "green", "F", "kind", "yes");
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES (
+    "Soon", 2, "pink", "F", "good-kinda-crazy", "yes");
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES (
+    "Jinho", 5, "brown", "M", "drunk?", "kinda");
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES (
+    "Hwuegyong", 4, "black", "F", "beyond-kind", "yes");
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES (
+    "Imyo", 100, "red", "F", "grumpy", NULL);
+INSERT INTO bears (name, age, color, gender, temperament, alive) VALUES (
+    "not-NULL", NULL, "black", "F", NULL, "yes");
