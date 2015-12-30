01949500e4bce2867884eea29a3b1fc0263b6c08
Richard Shen <rich.shen@nyu.edu>
2015-12-30 03:24:18 +0000

    added sql queries

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name) VALUES ("HARRY");
 INSERT INTO bears (name) VALUES ("BARRY");
 INSERT INTO bears (name) VALUES ("LARRY");
-INSERT INTO bears (name) VALUES ("TARRY");
+INSERT INTO bears (name) VALUES ("PARRY");
 INSERT INTO bears (name) VALUES ("WARRY");
 INSERT INTO bears (name) VALUES ("KARRY");
 INSERT INTO bears (name) VALUES ("PARRY");
