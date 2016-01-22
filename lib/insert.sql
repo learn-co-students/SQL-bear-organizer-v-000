e6a62e824daab33a69564dafc18bd5e56fea60a2
lamass <hadzy@yahoo.com>
2016-01-22 21:50:17 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) values
   ("Mr. Chocolate", 23, "F", "Light",     "fierce",     1),
-  ("Rowdy",         21, "M", "Light",     "whatever",   0),
-  ("Tabitha",       11,"F", "Rose",       "funny",     1),
+  ("Rowdy",   21, "M", "Light",     "whatever",   0),
+  ("Tabitha",    11,"F", "Rose",       "funny",     1),
   ("Sargeant Brown", 1, "M", "Yellow",   "funny",     1),
-  ("Melissa",       13, "F", "Brown",     "goofy",     1),
-  ("Grinch",        2, "F", "Black",     "grinchy",     1),
-  ("Wendy",         6, "F", "Brown",     "goofy",     0),
-  (NULL ,           20, "M", "black",     "aggressive",    0);
+  ("Melissa",    13, "F", "Brown",     "goofy",     1),
+  ("Grinch",     2, "F", "Black",     "grinchy",     1),
+  ("Wendy",    6, "F", "Brown",     "goofy",     0),
+  (NULL ,     20, "M", "black",     "aggressive",    0);
