2d94fb44ab7f91e6cb561efb1e486e575391e9c0
Rafael A. Mejia <rafael@dataminded.net>
2016-01-30 05:50:16 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+Create table bears (
+name text,
+age integer,
+gender char(1),
+color text,
+temperament text,
+alive boolean,
+id INTEGER,
+primary key (id asc))
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears values ("Mr. Chocolate",1,'M',"black","happy",1,1);
+insert into bears values ("Grinch",1,'M',"black","happy",1,2);
+insert into bears values ("Rowdy",1,'M',"black","happy",1,3);
+insert into bears values ("Tabitha",1,'M',"black","happy",1,4);
+insert into bears values ("Sergeant Brown",1,'M',"black","happy",1,5);
+insert into bears values ("Melissa",1,'M',"black","happy",1,6);
+insert into bears values ("Wendy",1,'M',"black","happy",1,7);
+insert into bears values (null,1,'M',"black","happy",1,8);
