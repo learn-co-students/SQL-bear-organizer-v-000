22419f0c6d77f95423349cafad98795338566612
mystycs <mystycs@gmail.com>
2016-02-10 20:20:54 +0000

    changes

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate', '3', 'M', 'Brown', 'Playful', '0');
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Rowdy', '3', 'M', 'Brown', 'Playful', '0');
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Tabitha', '6', 'F', 'Black', 'Shy', '0');
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown', '8', 'M', 'Brown', 'The Boss', '0');
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown', '8', 'M', 'Brown', 'The Leader', '0');
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Melissa', '17', 'F', 'Black', 'Lazy', '0');
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Grinch', '15', 'M', 'Black', 'Grumpy', '0');
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Wendy', '11', 'F', 'Brown', 'Goofy', '0');
